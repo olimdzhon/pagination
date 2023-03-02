0b18967836a59e1ef13ddff4a52f60000ccf86e3
Olimdzhon <write2sd@mail.ru>
2023-03-02 16:50:50 +0500

    Initial Commit

---
+++ Pagination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pagination
+//
+//  Created by Олимджон Садыков on 02/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
